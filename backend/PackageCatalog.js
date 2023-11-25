558f65b158d347f1289276914daed076dad54141
Fadi Nimer <fadi_nimer@hotmail.com>
2023-11-25 12:22:41 -0500

    Packaging utility classes

--- backend/PackageCatalog.js
+++ backend/PackageCatalog.js
@@ -97,3 +97,40 @@ class Package{
     }
 }
 
+class Size{
+    constructor(lengthCm, widthCm, heightCm){
+        this._length = lengthCm;
+        this._width = widthCm;
+        this.height = heightCm;
+    }
+    getLengthCm(){
+        return this._length;
+    }
+    getWidthCm(){
+        return this._width;
+    }
+    getHeightCm(){
+        return this._height;
+    }
+    getLengthInch(){
+        return (this._length * 0.393701);
+    }
+    getWidthInch(){
+        return (this._width * 0.393701);
+    }
+    getHeightInch(){
+        return (this._height * 0.393701);
+    }
+}
+
+class Weight{
+    constructor(weightKg){
+        this._weight = weightKg;
+    }
+    getWeightKg(){
+        return this._weight;
+    }
+    getWeightLbs(){
+        return (this._weight * 2.20462);
+    }
+}
